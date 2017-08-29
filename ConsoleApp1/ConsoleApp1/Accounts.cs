0ccbec74eb49b2460d13006ca04ac8bdb045030a
УважановЕ <@208-02-PC>
2017-08-29 18:15:34 +0600

    Account

---
+++ ConsoleApp1/ConsoleApp1/Accounts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    public class Accounts
+    {
+
+        public int SelectionSum { get; set; }
+        public double AvailabelAmount { get; set; }
+        public string AccountNumber { get; set; }
+        public string Sum { get; set; }
+        public int GetMoney()
+        {
+            return SelectionSum;
+        }
+        public void PutMoney(double x)
+        {
+            Console.WriteLine("Вы пополнили счет на сумму {0}", x);
+        }
+        public void ShowAmountMoney(double x)
+        {
+            AvailabelAmount = AvailabelAmount + x;
+            Console.WriteLine("Сумма на вашем счете состовляет {0}", AvailabelAmount);
+        }
+    }
+}
