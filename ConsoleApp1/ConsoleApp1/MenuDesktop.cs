401c57a18eb3c95d2aaaa40a3d3dc18a35d507cb
SagYernar <sagyernar@gmail.com>
2017-08-29 18:22:38 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/MenuDesktop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MenuDesktop
+    {
+        public void Handler()
+        {
+            Console.WriteLine("Введите карточку");
+            User user = new User();
+            Card card = new Card();
+            string strWrittedPassword;
+            int writtedPassword;
+
+            for (;;)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите PIN");
+                strWrittedPassword = Console.ReadLine();
+
+                if(Int32.TryParse(strWrittedPassword,out writtedPassword))
+                {
+                    var cardControll = user.Cards.Where(c => c.PIN == writtedPassword);
+                }
+
+                
+
+            }
+        }
+    }
+}
