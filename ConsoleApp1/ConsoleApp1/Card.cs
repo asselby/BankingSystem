a1dcb8ad9df9f3f05c5822f17126bc178b782994
asselby <asselby@users.noreply.github.com>
2017-08-29 18:23:25 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace BankingSystem
+{
+    public class Card
+    {
+        public string IdentificationNUmber { get; set; }
+        public bool IsValid { get; set; }
+        public int SecurityNumber { get; set; }
+        private int PIN { get; set; }
+
+        public bool isBlocked { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UsersBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    class UsersBase
+    {
+        User user = new User()
+        {
+
+        };
+
+        public void Dump() {
+
+            user = new User();
+
+            Card card = new Card();
+            card.IdentificationNUmber = "1234 4567 7890 0123";
+
+            user.Cards.Add(card);
+        }
+    }
+}
