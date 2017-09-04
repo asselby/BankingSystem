3cee66b25ee7860dae636b2f4bc7b7a6e59a60ac
Sabina <sabina.orunbaeva@gmail.com>
2017-09-04 13:55:35 +0600

    EmailSender Downloaded

---
+++ ConsoleApp1/ConsoleApp1/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace BankingSystem
+{
+    public class EmailSender
+    {
+        private string smtpHost;
+        private int smtpPort;
+        private string smtpUserName;
+        private string smtpUserPassword;
+        
+        public EmailSender()
+        {
+            smtpHost = "smtp.gmail.com";
+            smtpPort = 587;
+            smtpUserName = "162sdp@gmail.com";
+            smtpUserPassword = "124578369qwerty";
+        }
+
+        public void Send(string email, string messageSubject, string messageBody)
+        {
+            //Создание подключения
+            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+            client.Credentials = new NetworkCredential(smtpUserName, smtpUserPassword);
+            client.EnableSsl = true;
+
+            //Создание сообщения
+            MailMessage message = new MailMessage(smtpUserName, email, messageSubject, messageBody);
+
+            try
+            {
+                //Отсылаем сообщение
+                client.Send(message);
+            }
+
+            catch (SmtpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
